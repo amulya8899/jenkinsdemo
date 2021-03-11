3966bb8fb1065bb6c71c22046b05b8cd855aae9e
Amulya <marlapudiamulya888@gmail.com>
2021-03-11 15:26:15 +0530

    first commit

---
+++ src/main/java/com/zensar/servlets/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.zensar.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("testing");
+		String user=request.getParameter("uname");
+		String pass=request.getParameter("password");
+		if(user.contentEquals("zensar") && pass.contentEquals("zensar")) {
+			response.getWriter().print("Authentication Successfull!!!");
+		}else {
+			response.getWriter().print("Authentication Failed!!!");
+			}
+	}
+
+}
